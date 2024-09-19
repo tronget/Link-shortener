2c90efe325c3957b6acb8114229ee5032453e814
tronget <andrejlashkul@gmail.com>
2024-09-19 22:43:20 +0300

    added loader to client, redirected link on server when error occurs

---
+++ frontend/LinkShortener/src/components/Loader.js
@@ -0,0 +1,9 @@
+import CircularProgress from '@mui/material/CircularProgress';
+
+export default function Loader() {
+    return (
+        <div style={{marginTop: 10}}>
+            <CircularProgress/>
+        </div>
+    )
+}
--- frontend/LinkShortener/src/components/OneInputForm.js
+++ frontend/LinkShortener/src/components/OneInputForm.js
@@ -1,42 +1,56 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { isValidUrl } from "../functions/functions";
+import Loader from "./Loader";
 
-export default function OneInputForm({info="info"}) {
+export default function OneInputForm() {
     const { register, handleSubmit } = useForm();
 	const [data, setData] = useState("");
 	const [error, setError] = useState("");
+	const [loading, setLoading] = useState(false);
 
-	const reset = () => {
-		setData("");
-		setError("");
-	}
+    const reset = () => {
+        setData("");
+        setError("");
+        setLoading(false);
+    }
 
     return (
         <form onSubmit={handleSubmit(async (form) => {
-			reset();
-			const url = form.url;
-			if (!isValidUrl(url)) {
-				setError("Вы передали не ссылку!");
-				return;
-			}
-			const response = await fetch("https://shrtner-api.onrender.com", {
-				method: "POST",
-				headers: {
-					"Content-Type": "plain/text;charset=utf-8"
-				},
-				body: form.url
-			});
-			if (!response.ok) {
-				setError("Возникла ошибка при обработке запроса!");
-				console.error(response);
-				return;
-			}
-			const responseData = await response.text();
-			setData(responseData);
+            reset();
+            const url = form.url;
+            if (!isValidUrl(url)) {
+                setError("Вы передали не ссылку!");
+                return;
+            }
+
+            setLoading(true);
+
+            try {
+                const response = await fetch("https://shrtner-api.onrender.com", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "plain/text;charset=utf-8"
+                    },
+                    body: form.url
+                });
+                
+                if (!response.ok) {
+                    setError("Возникла ошибка при обработке запроса!");
+                    console.error(response);
+                    return;
+                }
+                const responseData = await response.text();
+                setData(responseData);
+            } catch (error) {
+                setError("Произошла ошибка: " + error.message);
+            } finally {
+                setLoading(false);
+            }
 		})}>
             <input {...register("url")} />
             <input type="submit" />
+			{loading && <Loader/>}
 			{data !== "" ? <p>Ваша ссылка: <a href={data}>{data}</a></p> : null}
 			{error !== "" ? <p style={{color: "red"}}>{error}</p> : null}
         </form>
